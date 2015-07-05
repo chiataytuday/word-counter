8ebc2eb7efbce7c2ae790d8d2bff9a0555d0d401
Jason Ho <eflyjason@gmail.com>
2015-07-05 14:42:59 +0800

    Initial Commit

---
+++ WordCounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WordCounter
+//
+//  Created by Arefly on 5/7/2015.
+//  Copyright (c) 2015年 Arefly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
