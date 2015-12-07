312531248d68148662641a29383556ff4c1fd910
Jason Ho <eflyjason@gmail.com>
2015-12-07 14:59:20 +0800

    - Optimize Today Extension

--- WordCounterTodayWidget/TodayViewController.swift
+++ WordCounterTodayWidget/TodayViewController.swift
@@ -25,16 +25,15 @@ class TodayViewController: UIViewController, UITextViewDelegate, NCWidgetProvidi
         print("[提示] Today View Controller 之 super.viewDidLoad() 已加載")
         
         self.view.translatesAutoresizingMaskIntoConstraints = false
+        self.view.userInteractionEnabled = true
         
-        //self.view.setTranslatesAutoresizingMaskIntoConstraints(false)
-        //textView.setTranslatesAutoresizingMaskIntoConstraints(false)
-        //self.view.sizeToFit()
-        //textView.sizeToFit()
-        
-        //textView.selectable = false
         textView.delegate = self
-        //textView.userInteractionEnabled = true
-        //textView.becomeFirstResponder()
+        textView.userInteractionEnabled = true
+        
+        textView.selectable = true  //BUG IN APPLE SIDE
+        textView.textColor = UIColor.whiteColor()
+        textView.selectable = false
+
         
         let tapGesture = UITapGestureRecognizer(target: self, action: "pressedOnce")
         self.view.addGestureRecognizer(tapGesture)
@@ -60,42 +59,36 @@ class TodayViewController: UIViewController, UITextViewDelegate, NCWidgetProvidi
             paraPlural = NSLocalizedString("Global.Units.Paragraph.Plural", comment: "paragraphs")
         }
         
-        //var clipBoard = UIPasteboard.generalPasteboard().string
-        
-        /*
-        textView.text = "XDD"
-        textView.textColor = UIColor.whiteColor()
-        //textView.font = UIFont(name: textView.font.fontName, size: 14)*/
         
         if let clipBoard = UIPasteboard.generalPasteboard().string {
             print("[提示] 已獲取用戶剪貼簿內容：\(clipBoard)")
             
             textView.text = clipBoard
-            
-            let wordCounts = wordCounterClass.wordCount(textView.text)
-            let wordWords = (wordCounts == 1) ? wordSingular : wordPlural
-            let wordTitle = String.localizedStringWithFormat(NSLocalizedString("Global.Count.Text.Word", comment: "%1$@ %2$@"), String(wordCounts), wordWords)
-            
-            let charCount = wordCounterClass.characterCount(textView.text)
-            let charWords = (charCount == 1) ? charSingular : charPlural
-            let charTitle = String.localizedStringWithFormat(NSLocalizedString("Global.Count.Text.Character", comment: "%1$@ %2$@"), String(charCount), charWords)
-            
-            let paraCount = wordCounterClass.paragraphCount(textView.text)
-            let paraWords = (paraCount == 1) ? paraSingular : paraPlural
-            let paraTitle = String.localizedStringWithFormat(NSLocalizedString("Global.Count.Text.Paragraph", comment: "%1$@ %2$@"), String(paraCount), paraWords)
-            
-            wordsCountLabel.text = wordTitle
-            parasCountLabel.text = paraTitle
-            charsCountLabel.text = charTitle
         }else{
             print("[提示] 用戶剪貼簿內並未任何內容")
             
             textView.text = NSLocalizedString("Global.Text.NothingOnClipboard", comment: "Nothing in your clipboard!")
             
-            wordsCountLabel.text = String.localizedStringWithFormat(NSLocalizedString("Global.Count.Text.Word.Zero", comment: "0 %@<-words"), wordPlural)
+            /*wordsCountLabel.text = String.localizedStringWithFormat(NSLocalizedString("Global.Count.Text.Word.Zero", comment: "0 %@<-words"), wordPlural)
             parasCountLabel.text = String.localizedStringWithFormat(NSLocalizedString("Global.Count.Text.Paragraph.Zero", comment: "0 %@<-paragraphs"), paraPlural)
-            charsCountLabel.text = String.localizedStringWithFormat(NSLocalizedString("Global.Count.Text.Character.Zero", comment: "0 %@<-characters"), charPlural)
+            charsCountLabel.text = String.localizedStringWithFormat(NSLocalizedString("Global.Count.Text.Character.Zero", comment: "0 %@<-characters"), charPlural)*/
         }
+        
+        let wordCounts = wordCounterClass.wordCount(textView.text)
+        let wordWords = (wordCounts == 1) ? wordSingular : wordPlural
+        let wordTitle = String.localizedStringWithFormat(NSLocalizedString("Global.Count.Text.Word", comment: "%1$@ %2$@"), String(wordCounts), wordWords)
+        
+        let charCount = wordCounterClass.characterCount(textView.text)
+        let charWords = (charCount == 1) ? charSingular : charPlural
+        let charTitle = String.localizedStringWithFormat(NSLocalizedString("Global.Count.Text.Character", comment: "%1$@ %2$@"), String(charCount), charWords)
+        
+        let paraCount = wordCounterClass.paragraphCount(textView.text)
+        let paraWords = (paraCount == 1) ? paraSingular : paraPlural
+        let paraTitle = String.localizedStringWithFormat(NSLocalizedString("Global.Count.Text.Paragraph", comment: "%1$@ %2$@"), String(paraCount), paraWords)
+        
+        wordsCountLabel.text = wordTitle
+        parasCountLabel.text = paraTitle
+        charsCountLabel.text = charTitle
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -122,7 +115,6 @@ class TodayViewController: UIViewController, UITextViewDelegate, NCWidgetProvidi
     func widgetMarginInsetsForProposedMarginInsets(defaultMarginInsets: UIEdgeInsets) -> UIEdgeInsets{
         //return UIEdgeInsetsZero
         return UIEdgeInsetsMake(10, 10, 10, 10)
-        //return UIEdgeInsetsMake(5, 0, 5, -10)
     }
     
     func pressedOnce() {
