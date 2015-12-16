77fdc5faf5e721626ef04496f1be94c88e6ee953
Jason Ho <eflyjason@gmail.com>
2015-12-16 21:52:50 +0800

    - Add pods

---
+++ Pods/Target Support Files/Pods-CounterActionExtension/Pods-CounterActionExtension-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CounterActionExtension : NSObject
+@end
+@implementation PodsDummy_Pods_CounterActionExtension
+@end
---
+++ Pods/Target Support Files/Pods-WordCounter/Pods-WordCounter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WordCounter : NSObject
+@end
+@implementation PodsDummy_Pods_WordCounter
+@end
