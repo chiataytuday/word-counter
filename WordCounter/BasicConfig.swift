1312c4f76aaf1be5f7a26017d539b115689f0aa1
Yifei He <yifei@hesyifei.com>
2020-09-07 19:00:12 +0800

    加入?action=write-review

--- WordCounter/BasicConfig.swift
+++ WordCounter/BasicConfig.swift
@@ -12,9 +12,9 @@ import Foundation
 class BasicConfig {
 	internal static let appStoreShortUrl = URL(string: "http://bit.ly/WordCounter")
     
-    // TODO: TRY USING THE NEW REVIEW SDK
     // https://stackoverflow.com/a/55876205/2603230
-	internal static let appStoreReviewUrl = URL(string: "https://itunes.apple.com/us/app/word-counter-tools/id1019068052")
+    // https://developer.apple.com/documentation/storekit/skstorereviewcontroller/2851536-requestreview
+	internal static let appStoreReviewUrl = URL(string: "https://itunes.apple.com/us/app/word-counter-tools/id1019068052?action=write-review")
 	internal static let otherAppsByMe = URL(string: "https://itunes.apple.com/us/developer/xueqin-huang/id1016182704")
 	//"itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=1019068052&onlyLatestVersion=true&pageNumber=0&sortOrdering=1)"
 
